415087c734e2884f4517c2f9e0811b4eda9f9504
saasbook <saasbook@saasbook.(none)>
2014-02-11 14:04:36 -0700

    updating currency

--- Currency.rb
+++ Currency.rb
@@ -1,6 +1,6 @@
 class Numeric
  @@currencies = {'dollar' => 1.0, 'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019}
- def method_missing(method_id, *args, &block)
+ def method_missing(method_id)
    singular_currency = method_id.to_s.gsub( /s$/, '')
    if @@currencies.has_key?(singular_currency) #Multiplies amount by the currency is it initially in
      self * @@currencies[singular_currency]
